5670f7d1a05109f89913c1785d6e0f7bfb263c70
Pavel Averin <pasha2281337alfa@gmail.com>
2024-09-25 01:52:31 +0300

    feat: add ldap connecetion and entries access

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using Novell.Directory.Ldap;
+
+string ldapHost = "ad.pu.ru";
+int ldapPort = 389; 
+string username = "";
+string password = ""; 
+string searchBase = "DC=ad,DC=pu,DC=ru";
+string searchFilter = "(&(objectClass=person)(memberOf=CN=АкадемГруппа_22.Б11-мм,OU=АкадемГруппа,OU=Группы,DC=ad,DC=pu,DC=ru))";
+
+var connection = new LdapConnection();
+connection.Connect(ldapHost, ldapPort);
+connection.Bind(username, password);
+
+var results = connection.Search(
+    searchBase,
+    LdapConnection.SCOPE_SUB,
+    searchFilter,
+    new[] { "sAMAccountType" }, 
+    false
+);
+
+while (results.hasMore())
+{
+    var entry = results.next();
+    Console.WriteLine(entry.getAttribute("sAMAccountType").StringValue);
+}
+
+connection.Disconnect();
+
